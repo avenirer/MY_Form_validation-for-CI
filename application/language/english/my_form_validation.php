8b0269a6b974e944542fe51f74712c47b9332912
Adrian Voicu <avenir.ro@gmail.com>
2016-11-28 16:56:31 +0200

    Create my_form_validation.php

---
+++ application/language/english/my_form_validation.php
@@ -0,0 +1,6 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['form_validation_datetime'] = 'The {field} field must have a DATETIME format.';
+$lang['form_validation_is_unique_except'] = 'The new {field} is not unique.';
+$lang['form_validation_phone_number'] = 'The {field} is not a valid phone number.';
+$lang['form_validation_email_domain'] = 'The {field} must have an accepted email domain';
