dc92f9595ce0cf12f49cc827d9459010f9bce9c1
Adrian Voicu <avenir.ro@gmail.com>
2017-02-24 12:23:40 +0200

    Update my_form_validation.php

--- application/language/english/my_form_validation.php
+++ application/language/english/my_form_validation.php
@@ -4,3 +4,4 @@ $lang['form_validation_datetime'] = 'The {field} field must have a DATETIME form
 $lang['form_validation_is_unique_except'] = 'The new {field} is not unique.';
 $lang['form_validation_phone_number'] = 'The {field} is not a valid phone number.';
 $lang['form_validation_email_domain'] = 'The {field} must have an accepted email domain';
+$lang['form_validation_not_in_list'] = 'The {field} value already exists.';
